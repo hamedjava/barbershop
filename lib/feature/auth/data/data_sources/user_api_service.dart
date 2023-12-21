abb116ef45d7959a4a0598b6098b1ff0f09460c8
hamedjava <hamedpouyanfar1987@gmail.com>
2023-12-21 11:49:48 +0330

    fix unused imports

--- lib/feature/auth/data/data_sources/user_api_service.dart
+++ lib/feature/auth/data/data_sources/user_api_service.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
-
 import 'package:retrofit/http.dart';
 import 'package:test/core/constant/constants.dart';
-import 'package:dio/dio.dart';
-import 'package:test/feature/auth/data/model/user.dart';
 
 //part 'user_api_service.g.dart';
 
